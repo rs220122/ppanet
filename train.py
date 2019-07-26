51c9180f5b26ac82127cda780ba1279ed96b485a
RyuatS <sitomi0216@icloud.com>
2019-07-26 13:48:57 +0900

    add print_flags method

--- train.py
+++ train.py
@@ -25,29 +25,50 @@ FLAGS = tf.app.flags.FLAGS
 #                            os.path.join('dataset', 'CamVid', 'tfrecord'),
 #                            'tfrecord directory')
 
+
+def print_args():
+    """ Print arguments. """
+    print('-' * 40)
+    print('flags information')
+    print('-' * 40)
+    keys = FLAGS.__flags.keys()
+    max_string_len = np.max([len(key) for key in keys])
+    string_format = '{:%d} : {}' % max_string_len
+    for key in keys:
+        if key in ['h', 'help', 'helpfull', 'helpshort']:
+            pass
+        else:
+            print(string_format.format(key, FLAGS[key].value))
+    print('-' * 40 + '\n')
+
+
 def main(argv):
+
+    print_args()
+
+    print('Training on %s %s set' % (FLAGS.split_name, FLAGS.dataset_name))
+
+    graph = tf.Graph()
     crop_size = [int(val) for val in FLAGS.crop_size]
-    print(crop_size)
+    with graph.as_default():
+        dataset = generator.Dataset(FLAGS.dataset_dir,
+                                    FLAGS.dataset_name,
+                                    FLAGS.split_name,
+                                    32,
+                                    crop_size,
+                                    min_resize_value=FLAGS.min_resize_value,
+                                    max_resize_value=FLAGS.max_resize_value,
+                                    resize_factor=FLAGS.resize_factor,
+                                    min_scale_factor=FLAGS.min_scale_factor,
+                                    max_scale_factor=FLAGS.max_scale_factor,
+                                    scale_factor_step_size=FLAGS.scale_factor_step_size,
+                                    is_training=True,
+                                    model_variant=FLAGS.model_variant,
+                                    should_shuffle=True)
 
-    dataset = generator.Dataset(FLAGS.dataset_dir,
-                                FLAGS.dataset_name,
-                                FLAGS.split_name,
-                                32,
-                                crop_size,
-                                min_resize_value=FLAGS.min_resize_value,
-                                max_resize_value=FLAGS.max_resize_value,
-                                resize_factor=FLAGS.resize_factor,
-                                min_scale_factor=FLAGS.min_scale_factor,
-                                max_scale_factor=FLAGS.max_scale_factor,
-                                scale_factor_step_size=FLAGS.scale_factor_step_size,
-                                is_training=True,
-                                model_variant=FLAGS.model_variant,
-                                should_shuffle=True)
+        iterator = dataset.get_one_shot_iterator()
+        samples  = iterator.get_next()
 
-    iterator = dataset.get_one_shot_iterator()
-    samples  = iterator.get_next()
-
-    
 
 
 if __name__ == '__main__':
