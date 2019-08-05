1ba65d06788625882e48c3efdaf6b4f9b157cb77
RyuatS <sitomi0216@icloud.com>
2019-08-05 12:37:25 +0900

    add bash file for train ppmnet

---
+++ ppm_train.sh
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+# ===============================================
+# Author: RyutaShitomi
+# date: 2019-08-03T14:06:05.435Z
+# Description:
+#
+# ===============================================
+
+python train.py --output_stride=8 \
+                --ppm_rates=1 \
+                --ppm_rates=2 \
+                --ppm_rates=3 \
+                --ppm_rates=6 \
+                --crop_size=512,512 \
+                --ppm_pooling_type=average \
