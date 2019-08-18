f184ee13876ad526701740a021dd10568d202e10
RyuatS <sitomi0216@icloud.com>
2019-08-18 14:49:47 +0900

    modified

--- ppm_train.sh
+++ ppm_train.sh
@@ -14,3 +14,10 @@ python train.py --output_stride=8 \
                 --ppm_rates=6 \
                 --crop_size=512,512 \
                 --ppm_pooling_type=average \
+                --batch_size=4             \
+                --model_variant=resnet_v1_50_beta \
+                --tf_initial_checkpoint=./backbone_ckpt/resnet_v1_50/model.ckpt \
+                --save_summaries_images \
+                --backbone_atrous_rate=1 \
+                --backbone_atrous_rate=2 \
+                --backbone_atrous_rate=4 
