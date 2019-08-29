427e98804abd124b7d3bd58f597c937de0d0c7ea
RyuatS <sitomi0216@icloud.com>
2019-08-29 16:53:47 +0900

    minor change

--- ppm_train.sh
+++ ppm_train.sh
@@ -6,23 +6,27 @@
 # Description:
 #
 # ===============================================
-TRAIN_LOGDIR='ppm_out_stride8'
-mkdir ${TRAIN_LOGDIR}
-nohup python train.py --output_stride=8 \
+NOW_DATE=`date '+%F'`
+TRAIN_LOGDIR='logs/ppm/log-'${NOW_DATE}
+mkdir -p ${TRAIN_LOGDIR}
+nohup python train.py \
+                --output_stride=16 \
                 --ppm_rates=1 \
                 --ppm_rates=2 \
                 --ppm_rates=3 \
                 --ppm_rates=6 \
                 --crop_size=512,512 \
-                --ppm_pooling_type=average \
-                --batch_size=4             \
+                --ppm_pooling_type=avg \
+                --batch_size=8             \
                 --model_variant=resnet_v1_50_beta \
                 --tf_initial_checkpoint=./backbone_ckpt/resnet_v1_50/model.ckpt \
                 --save_summaries_images \
-                --backbone_atrous_rate=1 \
-                --backbone_atrous_rate=2 \
-                --backbone_atrous_rate=4 \
+                --backbone_atrous_rates=1 \
+                --backbone_atrous_rates=2 \
+                --backbone_atrous_rates=4 \
                 --base_learning_rate=0.01 \
                 --weight_decay=0.0001 \
                 --train_logdir=${TRAIN_LOGDIR} \
-                --train_steps=100000 > ${TRAIN_LOGDIR}'/out.log'  &
+                --decoder_output_stride=4     \
+                --train_steps=10000 > ${TRAIN_LOGDIR}'/out.log'  &
+less +F ${TRAIN_LOGDIR}'/out.log'
