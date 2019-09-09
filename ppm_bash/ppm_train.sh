a83c76bc9feec30e38b6be4813386cba5dd1e9d3
RyuatS <sitomi0216@icloud.com>
2019-09-09 20:07:50 +0900

    minor change

--- ppm_bash/ppm_train.sh
+++ ppm_bash/ppm_train.sh
@@ -9,15 +9,19 @@
 NOW_DATE=`date '+%F'`
 TRAIN_LOGDIR='logs/ppm/log-'${NOW_DATE}
 mkdir -p ${TRAIN_LOGDIR}
+cp ppm_train.sh ${TRAIN_LOGDIR}'/ppm_train.sh'
 nohup python train.py \
-                --output_stride=16 \
+                --output_stride=8 \
                 --ppm_rates=1 \
                 --ppm_rates=2 \
                 --ppm_rates=3 \
                 --ppm_rates=6 \
-                --crop_size=512,512 \
+                --crop_size=360,480 \
+                --min_scale_factor=0.5 \
+                --max_scale_factor=2.0 \
+                --scale_factor_step_size=0.25 \
                 --ppm_pooling_type=avg \
-                --batch_size=8             \
+                --batch_size=8 \
                 --model_variant=resnet_v1_50_beta \
                 --tf_initial_checkpoint=./backbone_ckpt/resnet_v1_50/model.ckpt \
                 --save_summaries_images \
@@ -28,5 +32,5 @@ nohup python train.py \
                 --weight_decay=0.0001 \
                 --train_logdir=${TRAIN_LOGDIR} \
                 --decoder_output_stride=4     \
-                --train_steps=10000 > ${TRAIN_LOGDIR}'/out.log'  &
+                --train_steps=90000 > ${TRAIN_LOGDIR}'/out.log'  &
 less +F ${TRAIN_LOGDIR}'/out.log'
