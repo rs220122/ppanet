f52ec95a5da42929f28cc5cd87acd88ba47bb67f
Ryuta-S <sitomi0216@icloud.com>
2020-07-26 19:50:46 +0900

    add images for readme

--- create_video.py
+++ create_video.py
@@ -16,7 +16,7 @@ import cv2
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('log_dir',
+tf.app.flags.DEFINE_string('result_dir',
                            None,
                            'log directory.')
 
@@ -25,22 +25,18 @@ tf.app.flags.DEFINE_string('output_path',
                            'output file name.')
 
 def main(argv):
-    result_dir = os.path.join(FLAGS.log_dir, 'semantic_prediction_result')
     fnames = os.listdir(result_dir)
-    row_img_fnames = sorted([fname for fname in fnames if 'image' in fname])
-    row_pred_fnames = sorted([fname for fname in fnames if 'pred' in fname])
+    imgs = sorted(fnames)
 
-    height, width, _ = cv2.imread(os.path.join(result_dir, row_img_fnames[0])).shape
+    height, width, _ = cv2.imread(os.path.join(result_dir, imgs[0])).shape
     fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     out = cv2.VideoWriter(FLAGS.output_path, fourcc, 20.0, (width, height))
 
-    for img_fname, pred_fname in zip(row_img_fnames, row_pred_fnames):
+    for img_fname, pred_fname in zip(imgs, row_pred_fnames):
         img = cv2.imread(os.path.join(result_dir, img_fname))
-        pred = cv2.imread(os.path.join(result_dir, pred_fname))
 
-        weighted_img = cv2.addWeighted(img, 0.3, pred, 0.7, 1)
-        cv2.imshow('result', weighted_img)
-        out.write(weighted_img)
+        cv2.imshow('result', img)
+        out.write(img)
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
 
@@ -50,5 +46,5 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    tf.app.flags.mark_flag_as_required('log_dir')
+    tf.app.flags.mark_flag_as_required('result_dir')
     tf.app.run()
